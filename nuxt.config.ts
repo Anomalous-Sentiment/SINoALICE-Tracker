f7269e7352e2a0195ecd48acabfe93170cccc40a
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-03-19 18:39:23 +0800

    Changed primevue theme to viva dark

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
-        "primevue/resources/themes/lara-light-blue/theme.css",
+        "primevue/resources/themes/viva-dark/theme.css",
         "primevue/resources/primevue.css",
         "primeicons/primeicons.css"
     ],
