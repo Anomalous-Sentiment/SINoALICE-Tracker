4260f1c58939b58fc9e5e8b688b531bb1439a4dc
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-04-09 22:13:24 +0800

    Added a request to database to get list of timeslots

--- stores/guildStore.js
+++ stores/guildStore.js
@@ -3,18 +3,22 @@ import prisma from '../server/prisma.server.js'
 
 export const useGuildStore = defineStore('guilds', {
     state: () => ({
-        guilds: []
+        guilds: [],
+        timeslots: []
     }),
     getters: {
       alsoGuilds: (state) => state.guilds,
     },
     actions: {
       async populateGuilds() {
-        console.log('test')
         // Get the data from database and set state
-        // Use the view
-        const res =  await prisma.human_guild_list.findMany()
-        this.guilds = res
+        // Use the view to get guild data
+        const guildData =  await prisma.human_guild_list.findMany()
+        this.guilds = guildData
+
+        const timeslotData =  await prisma.timeslots.findMany()
+        console.log(timeslotData)
+        this.timeslots = timeslotData
       }
     },
 })
