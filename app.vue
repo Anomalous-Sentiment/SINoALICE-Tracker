87b2309fe55757939fb18e29ca5a232659165d65
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-04-11 00:31:59 +0800

    Added header and some CSS styling to table

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <div 
+  style="padding: 20px;
+    text-align: center;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background: #273238;
+    color:aliceblue;
+    font-size: 20px;
+    margin-bottom: 10px;
+    ">
+    <h1>SINoALICE Tracker</h1>
+  </div>
+  <div style="width: 80%; margin: 0 auto;">
     <Menubar :model="items">
       <template #start>
-    </template>
+      </template>
       <template #end>
         <Button label="Logout" @click="signOut" />
     </template>
--- pages/guilds.vue
+++ pages/guilds.vue
@@ -3,6 +3,8 @@
       <DataTable v-model:filters="filters" :value="guilds" paginator :rows="100" dataKey="Guild_ID" filterDisplay="row" sortField="ranking" :sortOrder="1"
         paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
         currentPageReportTemplate="{first} to {last} of {totalRecords}"
+        scrollable
+        scroll-height="1000px"
       >
         <template #header>
           <div class="flex justify-content-end">
@@ -19,12 +21,12 @@
                 {{ index + 1 }}
             </template>
         </Column>
-        <Column field="Overall_Rank" header="Overall Rank" sortable style="min-width: 12rem">
+        <Column field="Overall_Rank" header="Overall Rank" sortable>
             <template #body="{ data }">
                 {{ data['Overall_Rank'] }}
             </template>
         </Column>
-        <Column field="Guild" header="Guild Name" style="min-width: 12rem">
+        <Column field="Guild" header="Guild Name">
             <template #body="{ data }">
                 {{ data['Guild'] }}
             </template>
@@ -32,7 +34,7 @@
                 <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Search by name" />
             </template>
         </Column>
-        <Column field="Guild_Master" header="Guild Master" style="min-width: 12rem">
+        <Column field="Guild_Master" header="Guild Master">
             <template #body="{ data }">
                 {{ data['Guild_Master'] }}
             </template>
@@ -40,12 +42,12 @@
                 <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Search by name" />
             </template>
         </Column>
-        <Column field="Timeslot" header="Time Slot" filterField="Timeslot" :showFilterMenu="false" :filterMenuStyle="{ width: '7rem' }">
+        <Column field="Timeslot" header="Time Slot" filterField="Timeslot" :showFilterMenu="false" >
             <template #body="{ data }">
                 {{ data["Timeslot"] }}
             </template>
             <template #filter="{ filterModel, filterCallback }">
-              <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="timeslotFilters"  placeholder="All" class="p-column-filter" style="min-width: 7rem" :maxSelectedLabels="13">
+              <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="timeslotFilters"  placeholder="All" class="p-column-filter" :maxSelectedLabels="13">
                   <template #option="slotProps">
                       <div class="flex align-items-center gap-2">
                           <span>{{ slotProps.option }}</span>
@@ -54,22 +56,22 @@
               </MultiSelect>
                 </template>
         </Column>
-        <Column field="Wins" header="Wins" sortable style="min-width: 12rem">
+        <Column field="Wins" header="Wins" sortable>
             <template #body="{ data }">
                 {{ nf.format(data['Wins']) }}
             </template>
         </Column>
-        <Column field="Losses" header="Losses" sortable style="min-width: 12rem">
+        <Column field="Losses" header="Losses" sortable>
             <template #body="{ data }">
                 {{ nf.format(data['Losses']) }}
             </template>
         </Column>
-        <Column field="Draws" header="Draws" sortable style="min-width: 12rem">
+        <Column field="Draws" header="Draws" sortable>
             <template #body="{ data }">
                 {{ nf.format(data['Draws']) }}
             </template>
         </Column>
-        <Column field="Members" header="Members" sortable style="min-width: 12rem">
+        <Column field="Members" header="Members" sortable>
             <template #body="{ data }">
                 {{ data['Members'] }}
             </template>
@@ -84,7 +86,7 @@
                 {{ nf.format(data['Average_Member_CP']) }}
             </template>
         </Column>
-        <Column field="Median_Member_CP" header="Median Estimated CP" sortable style="min-width: 12rem">
+        <Column field="Median_Member_CP" header="Median Estimated CP" sortable>
             <template #body="{ data }">
                 {{ nf.format(data['Median_Member_CP']) }}
             </template>
