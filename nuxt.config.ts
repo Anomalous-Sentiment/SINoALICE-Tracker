98fc654e0a779fb1f396bd5096dfc5be9d979a4e
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-04-15 18:57:51 +0800

    Changed build output directory location to root or repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,9 +31,9 @@ export default defineNuxtConfig({
     },
     nitro: {
         output: {
-            dir: 'output',
-            serverDir: 'output/server',
-            publicDir: 'output/public'
+            dir: '~/output',
+            serverDir: '~/output/server',
+            publicDir: '~/output/public'
         }
     },
     
