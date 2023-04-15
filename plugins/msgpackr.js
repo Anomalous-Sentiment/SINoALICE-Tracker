1f04fba51a4f75802386003ed9998ba0afa10211
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-04-15 13:11:44 +0800

    Added try-catch block to unpack function

--- plugins/msgpackr.js
+++ plugins/msgpackr.js
@@ -3,9 +3,13 @@ export default defineNuxtPlugin(() => {
     return {
       provide: {
         unpack: (data) => {
+          try {
             const converted = new Uint8Array(data)
             const unpackedData = unpack(converted)
             return unpackedData
+          } catch (error) {
+            console.log(error.message)
+          }
         }
       }
     }
