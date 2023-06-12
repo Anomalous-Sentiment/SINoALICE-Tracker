4c62f7e702903c5ebd7c84bddfd3e0cad981467d
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-06-13 01:37:23 +0800

    Added page for displaying graph of player activity

---
+++ pages/player-activity.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <ClientOnly>
+            <apexchart type="area" :options="options" :series="series" ></apexchart>
+        </ClientOnly>
+    </div>
+</template>
+
+<script setup>
+import { useActivityStore } from '@/stores/activityStore.js'
+import { storeToRefs } from 'pinia'
+
+const activityStore = useActivityStore()
+
+const { playerActivity } = storeToRefs(activityStore)
+const { populateActivityStore } = activityStore
+
+const { pending } = useAsyncData('activity', async() => {
+    await populateActivityStore()
+  })
+
+const options = {
+    title: {
+        text: "SINoALICE Player Login Activity",
+        align: "left"
+    },
+    subtitle: {
+        text: "Logins by time period",
+        align: "left"
+    },
+    xaxis: {
+          type: 'datetime'
+    },
+    stroke: {
+        curve: 'straight'
+    },
+    tooltip: {
+        theme: 'dark',
+        x: {
+            show: true,
+            format: 'dd MMM yyyy',
+            formatter: undefined,
+        },
+    },
+    theme: {
+        mode: 'dark'
+    },
+    dataLabels: {
+            enabled: false
+    },
+}
+
+const series = [
+    {
+        name: "Logged in within 1 day",
+        data: playerActivity.value['day1Series']
+    },
+    {
+        name: "Logged in within 3 daya",
+        data: playerActivity.value['day3Series']
+    },
+    {
+        name: "Logged in within 5 daya",
+        data: playerActivity.value['day5Series']
+    },
+    {
+        name: "Logged in within 7 daya",
+        data: playerActivity.value['day7Series']
+    },
+    {
+        name: "Logged in within 14 daya",
+        data: playerActivity.value['day14Series']
+    },
+]
+</script>
