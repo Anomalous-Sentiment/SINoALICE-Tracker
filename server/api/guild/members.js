d96adbea07158d036a61261784516de7fd8e4b66
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-06-15 23:35:44 +0800

    Added API endpoints to access guild summary and member data

---
+++ server/api/guild/members.js
@@ -0,0 +1,53 @@
+import prisma from '../../prisma.server.ts'
+import { Packr } from 'msgpackr/pack'
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  try {
+    console.log('API call recieved for POST guild/members...')
+    const session = await getServerSession(event)
+    if (!session) {
+      return { status: 'unauthenticated' }
+    }
+
+    // Get the body
+    const body = await readBody(event)
+    const guildId = body['guildId']
+    
+    let start = Date.now()
+    
+    const membersPromise =  prisma.guild_members.findMany({
+        select: {
+          class_id: true,
+          member: true,
+          level: true,
+          estimated_cp: true,
+          current_cp: true
+        },
+        where: {
+            guild_id: guildId
+        },
+        orderBy: [
+          { estimated_cp: 'desc' }
+        ]
+    })
+  
+    const guildData = await membersPromise
+
+    let end = Date.now()
+
+    console.log(end - start)
+
+    start = Date.now()
+    const packr = new Packr({ mapsAsObjects: true, variableMapSize: true });
+    const packedData = packr.encode(guildData)
+    end = Date.now()
+
+    console.log(end - start)
+    event.node.res.setHeader('content-type', 'application/octet-stream')
+    event.node.res.end(packedData)
+  }
+  catch (err) {
+    console.log(err)
+  } 
+})
---
+++ server/api/guild/summary.post.js
@@ -0,0 +1,68 @@
+import prisma from '../../prisma.server.ts'
+import { Packr } from 'msgpackr/pack'
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  try {
+    console.log('API call recieved for POST summary...')
+    const session = await getServerSession(event)
+    if (!session) {
+      return { status: 'unauthenticated' }
+    }
+
+    // Get the body
+    const body = await readBody(event)
+    const guildId = body['guildId']
+    
+    let start = Date.now()
+    
+    const summaryPromise =  prisma.guild_summary.findUnique({
+        select: {
+            Guild_Name: true,
+            Guild_Master: true,
+            Ship_HP: true,
+            Ranking: true,
+            Rank: true,
+            Timeslot: true,
+            Description: true,
+            Recruitment_Msg: true,
+            Total_Estimated_CP: true
+        },
+        where: {
+            guilddataid: guildId
+        }
+    })
+  
+    const guildData = await summaryPromise
+    let convertedData = {}
+
+    convertedData['Guild Name'] = guildData['Guild_Name']
+    convertedData['Guild Master'] = guildData['Guild_Master']
+    convertedData['Ship HP'] = guildData['Ship_HP']
+    convertedData['Ranking'] = guildData['Ranking']
+    convertedData['Rank'] = guildData['Rank']
+    convertedData['Timeslot'] = guildData['Timeslot']
+    convertedData['Description'] = guildData['Description']
+    convertedData['Recruitment Msg'] = guildData['Recruitment_Msg']
+    convertedData['Total Estimated CP'] = guildData['Total_Estimated_CP']
+
+    console.log(convertedData)
+
+
+    let end = Date.now()
+
+    console.log(end - start)
+
+    start = Date.now()
+    const packr = new Packr({ mapsAsObjects: true, variableMapSize: true });
+    const packedData = packr.encode(convertedData)
+    end = Date.now()
+
+    console.log(end - start)
+    event.node.res.setHeader('content-type', 'application/octet-stream')
+    event.node.res.end(packedData)
+  }
+  catch (err) {
+    console.log(err)
+  } 
+})
