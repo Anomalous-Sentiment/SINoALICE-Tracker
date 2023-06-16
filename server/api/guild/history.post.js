917b2318354c14f09c3309594fdc7ffd3b7f0113
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-06-17 02:53:00 +0800

    Modified history endpoint to only return data if recaptcha score greater than 0.5

--- server/api/guild/history.post.js
+++ server/api/guild/history.post.js
@@ -9,39 +9,52 @@ export default defineEventHandler(async (event) => {
     // Get the body
     const body = await readBody(event)
     const guildId = body['guildId']
-    
-    let start = Date.now()
-    
-    const historyPromise =  prisma.guild_gc_history.findMany({
-        select: {
-          gc_num: true,
-          member_num: true,
-          timeslot: true,
-          lifeforce: true,
-          ranking: true,
-          ts_ranking: true,
-          wins: true
-        },
-        where: {
-            guilddataid: guildId
-        },
-        orderBy: [
-          { gc_num: 'desc' }
-        ]
+    const token = body['token']
+
+    const params = new URLSearchParams({
+      secret: useRuntimeConfig().secretCaptchaKey,
+      response: token
     })
+    const res = await fetch('https://www.google.com/recaptcha/api/siteverify?' + params, {
+      method: 'POST'
+    }).then(res => res.json())
+    console.log(res)
+
+    let guildData = []
+
+    // Check score to see if real user making request
+    if (res['success'] && res['score'] > 0.5)
+    {
+      let start = Date.now()
+    
+      const historyPromise =  prisma.guild_gc_history.findMany({
+          select: {
+            gc_num: true,
+            member_num: true,
+            timeslot: true,
+            lifeforce: true,
+            ranking: true,
+            ts_ranking: true,
+            wins: true
+          },
+          where: {
+              guilddataid: guildId
+          },
+          orderBy: [
+            { gc_num: 'desc' }
+          ]
+      })
+    
+      guildData = await historyPromise
   
-    const guildData = await historyPromise
-
-    let end = Date.now()
-
-    console.log(end - start)
-
-    start = Date.now()
+      let end = Date.now()
+  
+      console.log(end - start)
+    }
+    
     const packr = new Packr({ mapsAsObjects: true, variableMapSize: true });
     const packedData = packr.encode(guildData)
-    end = Date.now()
 
-    console.log(end - start)
     event.node.res.setHeader('content-type', 'application/octet-stream')
     event.node.res.end(packedData)
   }
