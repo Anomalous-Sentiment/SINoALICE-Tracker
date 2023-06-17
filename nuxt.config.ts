153be6d9e829a85489665aa9997461e46d74a921
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-06-17 21:23:44 +0800

    Explicitly set origin to ENV variable in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,7 @@ export default defineNuxtConfig({
         }
     },
     auth: {
+        origin: process.env.AUTH_ORIGIN,
         // Not sure if this does anything. Doesn't change anything in my testing (isEnabled)
         isEnabled: true,
         // This is named "globalAppMiddleware" incorrectly in the docs. This is the correct name
